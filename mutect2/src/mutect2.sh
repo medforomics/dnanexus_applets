a0b3d6dbe960306d38bfdceb2e1a143a211171bb
Ubuntu <JAMKuttan@dnanexusVM.esififiavqkelp4wjdp2wv2yag.jx.internal.cloudapp.net>
2019-11-12 16:02:11 +0000

    mutect2

--- mutect2/src/mutect2.sh
+++ mutect2/src/mutect2.sh
@@ -23,16 +23,19 @@ main() {
     # "$variable" --name".
 
     dx download "$GATK_tumor_bam" -o tumor.bam
+    dx download "$GATK_tumor_bam_index" -o tumor.bam.bai
     dx download "$GATK_normal_bam" -o normal.bam
+    dx download "$GATK_normal_bam_index" -o normal.bam.bai
+
     dx download "$ref_file" -o reference.tar.gz
 
     tar xvfz reference.tar.gz
     gunzip reference/genome.fa.gz
 
-    docker run -v ${PWD}:/data docker.io/goalconsortium/variantcalling:v1 java -XX:ParallelGCThreads=1 -Xmx16g -jar /usr/local/bin/picard.jar CollectSequencingArtifactMetrics I=tumor.bam O=artifact_metrics.txt R=reference/genome.fa
-    docker run -v ${PWD}:/data docker.io/goalconsortium/variantcalling:v1 sh -c "gatk --java-options \"-Xmx20g\" Mutect2 -R reference/genome.fa -A FisherStrand -A QualByDepth -A StrandArtifact -A DepthPerAlleleBySample -I tumor.bam -tumor ${pair_id} -I normal.bam -normal ${pair_id} --output ${pair_id}.mutect.vcf"
-    docker run -v ${PWD}:/data docker.io/goalconsortium/variantcalling:v1 sh -c "gatk --java-options \"-Xmx20g\" FilterMutectCalls -V ${pair_id}.mutect.vcf -O ${pair_id}.mutect.filt.vcf"
-    docker run -v ${PWD}:/data docker.io/goalconsortium/variantcalling:v1 sh -c "vcf-sort ${pair_id}.mutect.filt.vcf | vcf-annotate -n --fill-type | java -jar /usr/local/bin/snpEff/SnpSift.jar filter -p '(GEN[*].DP >= 10)' | bgzip > ${pair_id}.mutect.vcf.gz"
+    docker run -v ${PWD}:/data docker.io/goalconsortium/gatk:v1 java -XX:ParallelGCThreads=1 -Xmx16g -jar /usr/local/bin/picard.jar CollectSequencingArtifactMetrics I=tumor.bam O=artifact_metrics.txt R=reference/genome.fa
+    docker run -v ${PWD}:/data docker.io/goalconsortium/gatk:v1 sh -c "gatk --java-options \"-Xmx20g\" Mutect2 -R reference/genome.fa -A FisherStrand -A QualByDepth -A DepthPerAlleleBySample -I tumor.bam -tumor Tumor -I normal.bam -normal Normal --output ${pair_id}.mutect.vcf"
+    docker run -v ${PWD}:/data docker.io/goalconsortium/gatk:v1 sh -c "gatk --java-options \"-Xmx20g\" FilterMutectCalls -V ${pair_id}.mutect.vcf -R reference/genome.fa -O ${pair_id}.mutect.filt.vcf"
+    docker run -v ${PWD}:/data docker.io/goalconsortium/gatk:v1 sh -c "vcf-sort ${pair_id}.mutect.filt.vcf | vcf-annotate -n --fill-type | java -jar /usr/local/bin/snpEff/SnpSift.jar filter -p '(GEN[*].DP >= 10)' | bgzip > ${pair_id}.mutect.vcf.gz"
 
     mutect_vcf=$(dx upload ${pair_id}.mutect.vcf.gz --brief)
 
