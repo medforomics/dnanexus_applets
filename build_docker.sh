251af28c09ab15a7cca32d4fa43030b92dedfca3
Brandi Cantarel <b.cantarel@gmail.com>
2020-07-29 13:06:46 -0500

    update to 0.5.28 with school

--- build_docker.sh
+++ build_docker.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 cd docker
-ls | awk '{print "docker build --tag",$1":0.5.27",$1}' |sh
-ls | awk '{print "docker tag",$1":0.5.27 goalconsortium/"$1":0.5.27"}' |sh
-ls | awk '{print "docker push goalconsortium/"$1":0.5.27"}' |sh
+ls | awk '{print "docker build --tag",$1":0.5.28",$1}' |sh
+ls | awk '{print "docker tag",$1":0.5.28 goalconsortium/"$1":0.5.28"}' |sh
+ls | awk '{print "docker push goalconsortium/"$1":0.5.28"}' |sh
 
 source /Users/bcantarel/utsw/dnanexus/dx-toolkit/environment
-dx build -f align_markdups
-dx build -f geneabund
-dx build -f integratevcf
-dx build -f rna_bamqc
-dx build -f rnaalign
-dx build -f snv_indel_calling
-dx build -f star_fusion
-dx build -f sv_calling
 dx build -f trim_galore
+dx build -f align_markdups
+dx build -f snv_indel_calling
+dx build -f sv_calling
 dx build -f variant_profiling
+
+dx build -f geneabund
+dx build -f star_fusion
+dx build -f rna_bamqc
+
+
+dx build -f rnaalign
+dx build -f integratevcf
