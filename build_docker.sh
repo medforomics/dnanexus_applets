b48236667194412c2d89fbde67eb8d61fb260aa4
Brandi Cantarel <b.cantarel@gmail.com>
2020-08-12 21:34:40 -0500

    upgrade to 1.0.0

--- build_docker.sh
+++ build_docker.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
 
-perl -pi -e 's/version_0.5.36/version_0.5.40/g' docker/*/Dockerfile
-perl -pi -e 's/version_0.5.39/version_0.5.40/g' docker/*/Dockerfile
+perl -pi -e 's/version_0.5.40/version_1.0.0/g' docker/*/Dockerfile
+perl -pi -e 's/0.5.40/1.0.0/g' */src/* */dxapp.json
+
 
 cd docker
-ls | awk '{print "docker build --tag",$1":0.5.40",$1}' |sh
-ls | awk '{print "docker tag",$1":0.5.40 goalconsortium/"$1":0.5.36"}' |sh
-ls | awk '{print "docker push goalconsortium/"$1":0.5.40"}' |sh
+ls | awk '{print "docker build --tag",$1":1.0.0",$1}' |sh
+ls | awk '{print "docker tag",$1":1.0.0 goalconsortium/"$1":1.0.0"}' |sh
+ls | awk '{print "docker push goalconsortium/"$1":1.0.0"}' |sh
 cd ..
 
-perl -pi -e 's/0.5.36/0.5.36/g' */dxapp.json */src/*.sh build_docker.sh
 source /Users/bcantarel/utsw/dnanexus/dx-toolkit/environment
 ls */dxapp.json | awk -F '/' '{print "dx build -f",$1}' |sh
-dx build -f dalign
-dx build -f dna_bamqc
-dx build -f gatkbam
-dx build -f geneabund
-dx build -f integratevcf
-dx build -f markdups
-dx build -f rna_bamqc
-dx build -f rnaalign
-dx build -f snv_indel_calling
-dx build -f star_fusion
-dx build -f sv_calling
-dx build -f trim_galore
-dx build -f variant_profiling
-
