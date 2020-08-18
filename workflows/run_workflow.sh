288960f2c3c0b607a7ff4a56a08f7ee52d746854
Brandi Cantarel <brandi.cantarel@utsouthwestern.edu>
2020-08-17 21:51:15 -0500

    run_workflow runs all workflows

--- workflows/run_workflow.sh
+++ workflows/run_workflow.sh
@@ -3,12 +3,10 @@
 
 usage() {
   echo "-h Help documentation for gatkrunner.sh"
-  echo "-w  --projectID"
-  echo "-r  --Reference Genome: GRCh38 or GRCm38"
-  echo "-b  --baseDir script executable directory"
-  echo "-c  --processing dir"
-  echo "-x  --checkXML"
-  echo "Example: bash init_workflows.sh -p prefix -r /path/GRCh38 -b baseDir -c processingDir"
+  echo "-i  --input fastq directory"
+  echo "-d  --design designfile.txt"
+  echo "-w  --workflow DNA Nexus Workflow"
+  echo "Example: bash run_workflow.sh -i fastq_dir -d design.txt -w worflow_name"
   exit 1
 }
 OPTIND=1 # Reset OPTIND
@@ -23,35 +21,41 @@ do
 done
 
 shift $(($OPTIND -1))
-
 fqdir=$inputdir
-
 while read i; do
     line=($i)
     colnames=($(head -n 1 $design))
     opts=''
+    declare -A fqfiles
     while [ "${#colnames[@]}" -gt 0 ] ; do
 	eval ${colnames[0]}="${line[0]}"
-	if [[ ${colnames[0]} != 'FqR1' ]] && [[ ${colnames[0]} != 'FqR2' ]] && [[ ${colnames[0]} != 'RunID' ]] && [[ ${colnames[0]} != 'PanelFile' ]]
+	if [[ ! ${colnames[0]} =~ 'FqR' ]] && [[ ${colnames[0]} != 'RunID' ]] && [[ ${colnames[0]} != 'PanelFile' ]]
 	then
 	    opts="$opts -i${colnames[0]}=${line[0]}"
+	elif [[ ${colnames[0]} =~ 'FqR' ]]
+	then
+	    fqfiles[${colnames[0]}]=${line[0]}
 	fi
 	colnames=( "${colnames[@]:1}" )
 	line=( "${line[@]:1}" )
     done
     if [[ $CaseID != "CaseID" ]]
     then
-	read1="${fqdir}/$FqR1"
-	read2="${fqdir}/$FqR2"
 	if [[ -n $PanelFile ]]
 	then
 	    PanelFile="/referencedata/${PanelFile}.tar.gz"
 	fi
+	fqopt=''
 	dx mkdir -p /$RunID/$CaseID
-	FqR1=$(dx upload "$read1" --destination /$RunID/$CaseID/ --brief)
-	FqR2=$(dx upload "$read2" --destination /$RunID/$CaseID/ --brief)
-	runwkflow=$(dx run $wkflow $opts -iFqR1=$FqR1 -iFqR2=$FqR2 -iPanelFile=$PanelFile --destination /$RunID/$CaseID)
-	#echo "dx run $wkflow $opts -iFqR1=$FqR1 -iFqR2=$FqR2 -iPanelFile=$PanelFile --destination /$RunID/$CaseID";
+	for fq in "${!fqfiles[@]}"
+	do
+	    read="${fqdir}/${fqfiles[$fq]}"
+	    Fq=$(dx upload "$read" --destination /$RunID/$CaseID/ --brief)
+	    #Fq=$read
+	    fqopt="$fqopt -i${fq}=$Fq"
+	done
+	runwkflow=$(dx run $wkflow $opts $fqopt -iPanelFile=$PanelFile --destination /$RunID/$CaseID)
+	#echo "dx run $wkflow $opts $fqopt -iPanelFile=$PanelFile --destination /$RunID/$CaseID"
 	echo $opts $runwkflow >> ${wkflow}.joblist.txt
     fi 
 done <$design
