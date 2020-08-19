6584696a1738dc012e5b0bff862644f55bcdc30c
Brandi Cantarel <b.cantarel@gmail.com>
2020-08-19 11:34:19 -0500

    update monitorjobs.sh with email and download

--- workflows/monitor_jobs.sh
+++ workflows/monitor_jobs.sh
@@ -1,29 +1,55 @@
 #!/bin/bash
 #monitor_jobs.sh
 
+set -e
+
 jobbase='/project/PHG/PHG_Clinical/cloud'
+processing='/project/PHG/PHG_Clinical/processing'
+
 for i in ${jobbase}/pending/*.joblist.txt
 do
     filename="$(basename -- $i)"
     runid=$(echo $filename | cut -f 1 -d  '.')
     caseid=$(echo $filename | cut -f 2 -d  '.')
     complete=0
-    numjobs=$(wc -l demultiplex_dnanexus.sh |cut -f 1 -d ' ')
+    fail=0
+    numjobs=$(wc -l $i |cut -f 1 -d ' ')
+    cd ${processing}/${runid}/${caseid}
     while reads j
     do
 	dx describe $j --json > $j.json
-	#state=done
+	state=$(cat $j.json | jq -r '.state')
 	if [[ $state == 'done' ]]
 	then
 	    complete=$((complete+1))
-	elif [[ $state == 'failed' ]]
+	elif [[ $state == 'fail' ]]
 	then
-	    ##email ngsclialab@utsouthwestern.edu
+	    fail=1
+	    SUBJECT="SECUREMAIL: Case Job Failed: $j"
+            TO="ngsclialab@UTSouthwestern.edu,Chelsea.Raulerson@UTSouthwestern.edu"
+            email=$baseDir"/scripts/dnanexus_email.txt"
+	    cat $email | sed 's/Unspecified/'$j'/' | /bin/mail -s "$SUBJECT" "$TO"
 	fi
     done<$i
     if [[ $complete == $numjobs ]]
     then
+	mv $i $jobbase/complete
+	mkdir -p ${processing}/${runid}/${caseid}/analysis
+	cd ${processing}/${runid}/${caseid}/analysis
 	dx find data --path /${runid}/${caseid} --json > files.json
-	#exclude fastq and final.bam files
+	filemap=$(cat files.json | jq -c '.[] | [.describe.name, .describe.id]')
+	for i in $filemap
+	do
+	    line=$(echo $i | column -t -s'[],"')
+	    myarray=($line)
+	    if [[ ! ${myarray[0]} =~ 'fastq.gz' ]] && [[ ! ${myarray[0]} =~ 'final.ba' ]]
+	    then
+		echo "dx download ${myarray[1]}"
+	    else
+		echo "skipping ${myarray[0]}"
+	    fi
+	done
+    elif [[ $fail == 1 ]]
+	 mv $i $jobbase/archive
     fi
 done
